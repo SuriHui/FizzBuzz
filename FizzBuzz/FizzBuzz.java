07bbd96749f7037c451403e0b2ed1d9971f6f9e9
Suri Hui <suri.hui@mrjaffesclass.com>
2023-10-12 10:59:12 -0700

    test code

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -2,11 +2,22 @@ public class FizzBuzz
 {
     public static void main(String[] args)
     {
-        int a = 1;
-        int b = 5;
-        int c = 3;
-        for (a = 0; a <= 100; a++) {
-            System.out.println(a);
+        int Fizz = 0;
+        int Buzz = 0;
+        int i = 0;
+
+        if (Fizz == 3 && Buzz == 5) {
+            System.out.print("FizzBuzz");
+        } else if (Fizz == 3) {
+            System.out.print("Fizz");
+        } else if (Buzz == 5) {
+            System.out.print("Buzz");
+        } else {
+            System.out.print(i);
+        }
+
+        for (i = 0; i <= 100; i++) {
+            System.out.println(i);
         }
     }
 }
