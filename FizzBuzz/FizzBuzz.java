784f35790ef9b98f6671066c629d60c6b4cde266
Suri Hui <suri.hui@mrjaffesclass.com>
2023-10-13 11:09:18 -0700

    I got FizzBuzz working!

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -4,20 +4,26 @@ public class FizzBuzz
     {
         int Fizz = 0;
         int Buzz = 0;
-        int i = 0;
-
-        if (Fizz == 3 && Buzz == 5) {
-            System.out.print("FizzBuzz");
-        } else if (Fizz == 3) {
-            System.out.print("Fizz");
-        } else if (Buzz == 5) {
-            System.out.print("Buzz");
-        } else {
-            System.out.print(i);
-        }
-
-        for (i = 0; i <= 100; i++) {
-            System.out.println(i);
+        int i = 1;
+        
+        for (i = 1; i <= 100; i++) { 
+            Fizz++;
+            Buzz++;
+            if (Fizz == 3 && Buzz == 5) {
+                System.out.println("FizzBuzz");
+                Fizz = 0;
+                Buzz = 0;
+            }
+            if (Fizz == 3) {
+                System.out.println("Fizz");
+                Fizz = 0;
+            }
+            if (Buzz == 5) {
+                System.out.println("Buzz");
+                Buzz = 0;
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
